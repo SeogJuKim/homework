6cc4e6148d5407e72506816af362ba53821c011a
seogju04 <seogju04@gmail.com>
2025-09-30 22:24:46 +0900

    first

--- bin/hw1.dart
+++ bin/hw1.dart
@@ -1,6 +1,33 @@
 import 'dart:io';
 
 void main() {
-  print("Hello");
-  print("hello");
+
+  stdout.write("정수를 입력하세요: ");
+  String? input = stdin.readLineSync();
+
+  if (input == null || input.isEmpty) {
+    print("입력이 올바르지 않습니다.");
+    return;
+  }
+
+  int number = int.parse(input);
+
+  if (number < 0) {
+    print("$number -> $number");
+    return;
+  }
+
+  String numStr = number.toString();
+  List<String> digits = numStr.split('');
+
+  int sum = digits.map(int.parse).reduce((a, b) => a + b);
+
+  if (digits.length == 1) {
+    print("$number -> $number");
+  } else {
+    String expression = digits.join("+");
+    print("$number -> $expression = $sum");
+  }
 }
+
+
